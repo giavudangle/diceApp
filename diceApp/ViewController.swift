d1773eda5fe0e0db5fffc4649ae960b3ebd19fec
Vu Dang <kms.vudang@gmail.com>
2020-04-20 11:53:40 -0700

    done

--- diceApp/ViewController.swift
+++ diceApp/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to access UI element
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFive")
+
+    }
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // let is constant
+        // var can be change value
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        diceImageViewOne.image=diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image=diceArray[Int.random(in: 0...5)]
+       
     }
 
 
